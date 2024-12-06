b153992b2ac84b698528f3c08389d5c04ff8c6de
Juhani1104 <lucas710542@gmail.com>
2024-12-06 17:25:49 +0800

    Add a character

--- SourceCode/hero.cpp
+++ SourceCode/hero.cpp
@@ -10,62 +10,49 @@ namespace HeroSetting {
 	static constexpr char gif_root_path[40] = 
 		"./assets/gif/Hero";
 	static constexpr char gif_postfix[][10] = {
-		"left", "right", "front", "back"
+		"left", "right", "front"
 	};
 }
  void Hero::init(){
     for(size_t type=0 ;type<static_cast<size_t>(Herostate::herostatamax);type++){
     char buffer[50];
 	sprintf(
-		  buffer, "%s/dragonite_%s.gif",
+		buffer, "%s/dragonite_%s.gif",
 		HeroSetting::gif_root_path,
-		  HeroSetting::gif_postfix[static_cast<int>(type)]);
+		HeroSetting::gif_postfix[static_cast<int>(type)]);
         gifpath[static_cast<Herostate>(type)]=std::string(buffer);
-        
     }
     GIFCenter *GIFC = GIFCenter::get_instance();
-ALGIF_ANIMATION *gif = GIFC->get(gifpath [state]);
-DataCenter *DC = DataCenter::get_instance();
-shape.reset(new Rectangle{DC->window_width / 2,
-DC->window_height / 2,
-DC->window_width / 2 + gif->width,
-DC->window_height / 2 + gif->height});
+    ALGIF_ANIMATION *gif = GIFC->get(gifpath [state]);
+    DataCenter *DC = DataCenter::get_instance();
+    shape.reset(new Rectangle{DC->window_width / 2,
+    DC->window_height*0,
+    DC->window_width / 2 - gif->width*3,
+    DC->window_height*0 + gif->height*2});
  }
  
 		
-	void Hero::update()
+void Hero::update()
 {
     DataCenter *DC = DataCenter::get_instance();
-    if (DC->key_state[ALLEGRO_KEY_W])
-    {
-        shape->update_center_y(shape->center_y() - speed);
-        state = Herostate::back;
-    }
-    else if (DC->key_state[ALLEGRO_KEY_A])
+
+    if (DC->key_state[ALLEGRO_KEY_A]||DC->key_state[ALLEGRO_KEY_LEFT])
     {
         shape->update_center_x(shape->center_x() - speed);
         state = Herostate::left;
     }
-    else if (DC->key_state[ALLEGRO_KEY_S])
-    {
-        shape->update_center_y(shape->center_y() + speed);
-        state = Herostate::front;
-    }
-    else if (DC->key_state[ALLEGRO_KEY_D])
+    else if (DC->key_state[ALLEGRO_KEY_D]||DC->key_state[ALLEGRO_KEY_RIGHT])
     {
         shape->update_center_x(shape->center_x() + speed);
         state = Herostate::right;
     }
 }
 
-	void Hero::draw(){
+void Hero::draw(){
        
-GIFCenter *GIFC = GIFCenter::get_instance();
-ALGIF_ANIMATION *gif = GIFC->get(gifpath [state]);
-algif_draw_gif(gif,
-shape->center_x() - gif->width / 2,
-shape->center_y() - gif->height / 2, 0);
-
-
-
-    }
+    GIFCenter *GIFC = GIFCenter::get_instance();
+    ALGIF_ANIMATION *gif = GIFC->get(gifpath [state]);
+    algif_draw_gif(gif,
+    shape->center_x() - gif->width / 2,
+    shape->center_y() - gif->height / 2, 0);
+}
--- SourceCode/hero.h
+++ SourceCode/hero.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <map>
 enum class Herostate{
-	left, right, front , back,herostatamax
+	left, right, front,herostatamax
 };
 class Hero : public Object
 {
